44d65e62afe6273721a366cec8291bb3340cd5aa
Maksim Shushakov <shushakov.maxim@gmail.com>
2016-12-04 12:00:21 +0700

    added kanban board

---
+++ kanban/js/models/todo.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+    _timer: null,
+
+    defaults: {
+		title: "empty todo ...",
+		completed: false,
+		paused: true,
+		important: false,
+        position: {x: 0, y: 0},
+        periods: []
+	},
+
+    initialize: function() {
+        this._timer = Object.create(app.Timer);
+        this._timer.init(this.get("periods"));
+    },
+
+	start: function () {
+        this._timer.startPeriod();
+        this.change("periods", this._timer.periods);
+        this.change("pause", false);
+        this.change("completed", false);;
+	},
+
+	pause: function() {
+        if (!this.get("pause")) {
+            this._timer.endPeriod();
+            this.change("periods", this._timer.periods);
+            this.change("pause", true);
+        }
+	},
+
+	resume: function() {
+        this.start();
+	},
+
+	end: function() {
+        this.pause();
+        this.change("completed", true);
+	},
+
+    change: function(attr, value) {
+        var updates = {};
+        updates[attr] = value;
+        this.save(updates);
+    },
+
+    getTime: function() {
+        return this._timer.getTotalTime();
+    },
+
+    remove: function() {
+        this.destroy();
+    }
+})
