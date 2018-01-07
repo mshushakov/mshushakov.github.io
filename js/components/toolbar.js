d4866dcb9e85244f84b6c4003fcb7daff5f6ed99
Maxim Shushakov <shushakov.maxim@gmail.com>
2018-01-08 00:22:33 +0700

    Remove container for menu/back buttons in toolbar

--- js/components/toolbar.js
+++ js/components/toolbar.js
@@ -3,7 +3,7 @@ import { create } from '/js/tools.js';
 export function Toolbar(props) {
 	const element = (
 		create('div', { className: 'toolbar' },
-			create('div', { className: 'toolbar_icons' }),
+			//create('div', { className: 'toolbar_icons' }),
 			create('div', { className: 'toolbar_title', textContent: 'Dungeons & Dragons' }),
 			create('div', { className: 'toolbar_icons' }),
 		)
