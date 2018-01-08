7e1e46dd1be91f96f87763952ad910a0d1589145
Maxim Shushakov <shushakov.maxim@gmail.com>
2018-01-08 17:42:28 +0700

    Add  function for creating svg icons

--- js/tools.js
+++ js/tools.js
@@ -5,6 +5,13 @@ const create = (tag, props, ...children) => {
 	return element;
 }
 
+const createIcon = (shape, props, ...children) => {
+	const element = document.createElementNS("http://www.w3.org/2000/svg", shape);
+	if (props) Object.keys(props).forEach(prop => element.setAttribute(prop, props[prop]));
+	if (children) children.forEach(child => element.appendChild(child));
+	return element;
+}
+
 const asyncrender = (url, render, fail) => {
 	const element = create('div', { className: 'fragment' });
 
@@ -24,4 +31,4 @@ const asyncrender = (url, render, fail) => {
 	return element;
 }
 
-export { create, asyncrender }
+export { create, createIcon, asyncrender }
