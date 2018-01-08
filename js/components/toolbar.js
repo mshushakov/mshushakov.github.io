4385b58e8d4ac79cbc3a5bb44b4723eeaea117f3
Maxim Shushakov <shushakov.maxim@gmail.com>
2018-01-08 17:42:54 +0700

    Add menu icon on toolbar

--- js/components/toolbar.js
+++ js/components/toolbar.js
@@ -1,11 +1,19 @@
-import { create } from '/js/tools.js';
+import { create, createIcon } from '/js/tools.js';
+
+const icons = {
+	'menu': createIcon('svg', { viewBox: '0 0 24 24'},
+		createIcon('path', { d: 'M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z', fill: 'currentColor'})
+	),
+	'back': ''
+}
 
 export function Toolbar(props) {
 	const element = (
 		create('div', { className: 'toolbar' },
-			//create('div', { className: 'toolbar_icons' }),
-			create('div', { className: 'toolbar_title', textContent: 'Dungeons & Dragons' }),
-			create('div', { className: 'toolbar_icons' }),
+			create('div', { className: 'toolbar_icons' }, 
+				icons.menu
+			),
+			create('div', { className: 'toolbar_title', textContent: 'Dungeons & Dragons' })
 		)
 	);
 
