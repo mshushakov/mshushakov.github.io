7ba4dc5cd55175247e22b63ba20e569356e2f7c1
Maxim Shushakov <shushakov.maxim@gmail.com>
2018-01-25 23:46:41 +0700

    Update service worker and theme color

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
+const version = 'v1.0.0';
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1.0.1').then(function(cache) {
+    caches.open(version).then(function(cache) {
       return cache.addAll([
         '/',
         '/css/app.css',
@@ -12,9 +14,9 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.match('/api/')) {
+  if (event.request.url.match('/api/') || event.request.url.match('/js/')) {
    event.respondWith(
-    caches.open('v1').then(function(cache) {
+    caches.open(version).then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
